4636e668322d1f8ffa29ae64291042ebce9c6e24
Maxwell Morgan <mmorgan@linchpinagency.com>
2016-05-20 13:41:19 -0400

    Base gulp set-up

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp         = require( 'gulp' ),
+	sass         = require( 'gulp-sass' ),
+	autoprefixer = require( 'gulp-autoprefixer' ),
+	watch        = require( 'gulp-watch' );
+
+// Sass
+gulp.task( 'sass', function() {
+	return gulp.src('./scss/**/*.scss')
+		.pipe( sass({
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+		.pipe( gulp.dest('./css') );
+} );
+
+// Autoprefixer
+gulp.task( 'autoprefixer', ['sass'], function() {
+	return gulp.src( './css/*.css' )
+		.pipe( autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}) )
+		.pipe( gulp.dest('./css') );
+} );
+
+// Watch
+gulp.task( 'watch', ['sass'], function() {
+	gulp.watch('./scss/**/*.scss', ['sass', 'autoprefixer']);
+} );
+
+// Default "gulp" task
+gulp.task('default', ['sass', 'autoprefixer', 'watch']);

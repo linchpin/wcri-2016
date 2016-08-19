d5f6f8c9ab4c9c314b7f06ea60e1c1c63f8e0bd0
Maxwell Morgan <mmorgan@linchpinagency.com>
2016-08-19 16:52:49 -0400

    Initial commit of interior styles

---
+++ hero.php
@@ -0,0 +1,27 @@
+<div id="wcri-hero">
+	<div id="hero-content" class="text-center">
+		<img src="https://2016.rhodeisland.wordcamp.org/files/2016/06/wcri-logo-background.png" class="hero-content-logo" />
+		<p>
+			New England Tech, Sept 30 & Oct 1
+			<br />
+			<a href="#" class="button">Get Tickets</a>
+		</p>
+	</div>
+
+	<div id="clouds">
+		<span class="cloud cloud-1"></span>
+		<span class="cloud cloud-2"></span>
+		<span class="cloud cloud-3"></span>
+		<span class="cloud cloud-4"></span>
+		<span class="cloud cloud-5"></span>
+		<span class="cloud cloud-6"></span>
+	</div>
+
+	<div id="making-waves">
+		<img class="pellbridge" src="https://2016.rhodeisland.wordcamp.org/files/2016/08/pellbridge@2x.png" />
+		<img class="das-boat" src="https://2016.rhodeisland.wordcamp.org/files/2016/08/sailboat@2x.png" />
+		<img class="winston-the-whale" src="https://2016.rhodeisland.wordcamp.org/files/2016/08/winston-the-whale@2x.png" />
+		<div class="wave wave-1"></div>
+		<div class="wave wave-2"></div>
+	</div>
+</div>

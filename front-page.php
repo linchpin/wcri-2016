602850895f122faa58bb363f87aadaab7a7e5aca
Maxwell Morgan <mmorgan@linchpinagency.com>
2016-09-02 09:11:31 -0400

    Schedule styles, beginning to implement styles for new homepage layout

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * HTML to use for Front Page content
+ */
+?>
+
+
+<div id="homepage-buckets" class="row">
+	<div class="small-12 medium-4 columns">
+		<div class="bucket-content">
+			About
+		</div>
+	</div>
+
+	<div class="small-12 medium-4 columns">
+		<div class="bucket-content">
+			Schedule
+		</div>
+	</div>
+
+	<div class="small-12 medium-4 columns">
+		<div class="bucket-content">
+			Attendees
+		</div>
+	</div>
+</div>

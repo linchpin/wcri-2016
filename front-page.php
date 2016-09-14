1cf6ec5febfb8ef3143429efb1b6e8fe5e1719c5
Maxwell Morgan <mmorgan@linchpinagency.com>
2016-09-14 13:28:20 -0400

    Responsive styles, footer styles

--- front-page.php
+++ front-page.php
@@ -6,21 +6,56 @@
 
 
 <div id="homepage-buckets" class="row">
-	<div class="small-12 medium-4 columns">
-		<div class="bucket-content">
-			About
-		</div>
+	<div class="bucket"><div class="bucket-content"><h2>About</h2>
+			WordCamp RI is a non-profit conference, fully organized by local volunteers. The 2-day conference is designed to bring WordPress enthusiasts, business owners, engineers and bloggers together. Here they will learn, network and have fun with a group of like-minded individuals.
+
+			<p class="text-center"><a href="https://central.wordcamp.org/about/" target="_blank" class="button medium">About WordCamp</a></p></div></div>
+
+	<div class="bucket"><div class="bucket-content"><h2>Schedule</h2>
+			Our schedule is broken into three different tracks: Beginner, Intermediate and Advanced. In each track you will learn valuable information tailored to your needs. Each speaker is dedicated to helping the WordPress community grow & flourish. There are some crowd favorites coming back to share even more knowledge, as well as newcomers who we are very excited about!
+
+			<p class="text-center"><a href="/schedule" target="_blank" class="button medium">View the Schedule</a></p></div></div>
+
+	<div class="bucket"><div class="bucket-content"><h2>Sponsors</h2>
+			WordCamp RI is so grateful for the generosity of its sponsors. Their commitment to the WordPress community allows events like WordCamps to be so successful. With their support, we are able to offer 2 full days of training, lunch, networking, after party and more for only $20 per ticket.
+
+			<p class="text-center"><a href="/sponsors" target="_blank" class="button medium">View the Sponsors</a></p></div></div>
+</div>
+
+<div id="speakers-callout" class="row extend">
+	<div class="small-12 columns">
+		<h2>Keynote - John Maeda</h2>
+		<img src="https://2016.rhodeisland.wordcamp.org/files/2016/09/approved-photo-of-John-Maeda-.jpg" class="alignleft" style="width:300px;" />
+		<h3>Computational Design and Inclusion.</h3>
+		<p>John Maeda is an American executive spearheading a new convergence across the design + technology industries. He recently joined Automattic as Global Head of Computational Design + Inclusion and previously served as Design Partner at Kleiner Perkins Caufield & Byers, a world-leading venture capital firm. An internationally recognized speaker and author, his books include The Laws of Simplicity, Creative Code and Redesigning Leadership. He holds degrees in Electrical Engineering + Computer Science from MIT, an MBA from Arizona State U, and a PhD from University of Tsukuba in Japan.</p>
+	</div>
+</div>
+
+<div id="venues-callout">
+	<div class="small-12 medium-6 columns venue wcri-venue">
+		<span class="overlay"></span>
+		<h2>WCRI Venue</h2>
+		<p>New England Tech is a private, non-profit technical college located in Southeastern New England. Their state-of-art campus makes it the perfect choice when deciding to host WordCamp RI. Their staff and students have a willingness to help the local WordPress community. We are thrilled to be back at NEIT again for WordCamp 2016.</p>
+	</div>
+	<div class="small-12 medium-6 columns venue after-party-venue">
+		<span class="overlay"></span>
+		<h2>After Party</h2>
+		<p>The After-Party is a continuation of the WordCamp RI experience, one that shouldn’t be missed! Your WordCamp RI badge is your ticket in! That’s right; the food, drinks & fun are all included in your ticket price!  So don’t forget your badge and we hope to see you at Finn’s Saturday October 1st at 6pm!</p>
+	</div>
+</div>
+
+<div id="recent-blog-posts">
+	<div class="small-12 medium-6 columns main-post">
+		<h2>Our Fifth Year of WordCamp RI</h2>
+		<p>Our first WordCamp was held in 2012 at the University of Rhode Island’s Harrington School of Communication and Media in downtown Providence.  The lead organizer was Luke Gedeon. Jesse Friedman was on the WordCamp organizing committee.</p>
 	</div>
 
-	<div class="small-12 medium-4 columns">
-		<div class="bucket-content">
-			Schedule
-		</div>
-	</div>
-
-	<div class="small-12 medium-4 columns">
-		<div class="bucket-content">
-			Attendees
-		</div>
+	<div class="small-12 medium-6 columns recent-posts">
+		<ul>
+			<li>Thank You to Our Speakers</li>
+			<li>Don’t be afraid to ask questions!</li>
+			<li>Thank You Linchpin and Foster Grant</li>
+			<li>The After-Party</li>
+		</ul>
 	</div>
 </div>
